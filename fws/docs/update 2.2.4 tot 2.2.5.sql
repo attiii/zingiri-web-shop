5f2349ead193f10a380c1b72eddee75a2af20775
erikpbogaerts <erikpbogaerts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-14 19:21:28 +0000

    1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/zingiri-web-shop/trunk@182811 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fws/docs/update 2.2.4 tot 2.2.5.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `settings` CHANGE `theme` `use_captcha` TINYINT( 1 ) NULL DEFAULT NULL;
-UPDATE settings SET `use_captcha` = '1';
-ALTER TABLE `settings` ADD `use_imagepopup` TINYINT( 1 ) NULL ;
-UPDATE settings SET `use_imagepopup` = '1';
-ALTER TABLE `product` ADD `FEATURES` VARCHAR( 300 ) NULL ;
-ALTER TABLE `basket` CHANGE `DESCRIPTION` `FEATURES` VARCHAR( 255 ) NOT NULL 
