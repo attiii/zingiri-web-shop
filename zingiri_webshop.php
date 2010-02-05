c5aee03a22bae73066ad716e81af830fd502a166
erikpbogaerts <erikpbogaerts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-05 16:09:26 +0000

    1.2.6
    git-svn-id: https://plugins.svn.wordpress.org/zingiri-web-shop/trunk@202275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- zingiri_webshop.php
+++ zingiri_webshop.php
@@ -26,13 +26,14 @@
  Plugin URI: http://www.zingiri.com/WebShop
  Description: This plugin integrates the fabulous Free Web Shop e-commerce solution with Wordpress.
  Author: Erik Bogaerts
- Version: 1.2.5
+ Version: 1.2.6
  Author URI: http://www.zingiri.com/
  */
-define("ZING_VERSION","1.2.5");
+define("ZING_VERSION","1.2.6");
 require(dirname(__FILE__)."/zing.inc.php");
 
 if (file_exists(dirname(__FILE__).'/zap')) {
+	define("ZING_APPS_PLAYER_VERSION","0.7");
 	define("ZING_APPS",dirname(__FILE__)."/fws/fields/");
 	define("ZING_APPS_EMBED","zap/");
 	define("ZING_APPS_TRANSLATE",'z_');
