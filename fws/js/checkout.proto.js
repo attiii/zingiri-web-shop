f1f4594849866ecf0f0cae7d250d3d0147287389
erikpbogaerts <erikpbogaerts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-24 19:21:57 +0000

    1.6.3
    git-svn-id: https://plugins.svn.wordpress.org/zingiri-web-shop/trunk@293260 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fws/js/checkout.proto.js
+++
@@ -1,9 +0,0 @@
-function wsSubmit()
-{
-	// force load in same page, by default it will open a new page
-	var autosubmit = $('autosubmit');
-	if (autosubmit) {
-		autosubmit.target = '';
-		autosubmit.submit();
-	}
-}
--- fws/js/imagedisplay.proto.js
+++
@@ -1,8 +0,0 @@
-function wsHoverImage(img,height,width) {
-	var highlight=$('highlight_image');
-	highlight.src=img;
-	highlight.height=height;
-	highlight.width=width;
-	
-	if ($('highlight_ref') != null) $('highlight_ref').href=img;
-}
