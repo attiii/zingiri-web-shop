9b4459d76967d2338e9fcfcbe2fc41e90d963368
erikpbogaerts <erikpbogaerts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-07 09:46:45 +0000

    2.5.1
    git-svn-id: https://plugins.svn.wordpress.org/zingiri-web-shop/trunk@635296 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fws/js/src/checkout.jquery.js
+++
@@ -1,9 +0,0 @@
-function wsSubmit()
-{
-	// force load in same page, by default it will open a new page
-	var autosubmit = jQuery('#autosubmit');
-	if (autosubmit) {
-		autosubmit.attr('target','');
-		autosubmit.submit();
-	}
-}
