add1a8c0f2acec0e066c03c52262ab62745881c1
erikpbogaerts <erikpbogaerts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-19 05:38:09 +0000

    2.6.7
    git-svn-id: https://plugins.svn.wordpress.org/zingiri-web-shop/trunk@860674 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fwkfor/classes/sub.select.class.php
+++ fwkfor/classes/sub.select.class.php
@@ -1,33 +1,11 @@
 <?php
-/*  sub.select.class.php
- Copyright 2008,2009 Erik Bogaerts
- Support site: http://www.aphps.com
-
- This file is part of APhPS.
-
- APhPS is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- APhPS is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with APhPS; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-?>
-<?php
 class selectZfSubElement extends zfSubElement {
 
 	function output($mode="edit",$input="")
 	{
 		foreach ($this->xmlf->values->children() as $child) {
 			$t=(array)$child;
-			$option=isset($t[0]) ? $t[0] : '';
+			$option=isset($t[0]) ? $t[0] : $child->__toString();
 			if (isset($child->attributes()->value)) $value=$child->attributes()->value;
 			else $value=$option;
 			if ($value==$this->int) $this->ext=$option;
@@ -43,36 +21,40 @@ class selectZfSubElement extends zfSubElement {
 		if($e->populated_value['element_'.$e->id.'_'.$i] == ""){
 			$e->populated_value['element_'.$e->id.'_'.$i] = $xmlf->fields->{'field'.$i}->default;
 		}
-		$field_markup.="<select id=\"element_{$e->id}_{$i}{$this->ail}\" name=\"element_{$e->id}_{$i}\" class=\"element text\" style=\"width: {$xmlf->fields->{'field'.$i}->width}\" {$e->readonly}>";
-		$option_markup="";
-		if ($xmlf->fields->{'field'.$i}->values->attributes()->type=='multi') {
-			foreach ($xmlf->fields->{'field'.$i}->values->children() as $child) {
-				$t=(array)$child;
+		if (in_array($this->mode,array('edit','add','build','search'))) {
+			$field_markup.="<select id=\"element_{$e->id}_{$i}{$this->ail}\" name=\"element_{$e->id}_{$i}\" class=\"element text\" style=\"width: {$xmlf->fields->{'field'.$i}->width}\" {$e->readonly}>";
+			$option_markup="";
+			if ($xmlf->fields->{'field'.$i}->values->attributes()->type=='multi') {
+				foreach ($xmlf->fields->{'field'.$i}->values->children() as $child) {
+					$t=(array)$child;
 
-				$option=isset($t[0]) ? $t[0] : '';
-				if (isset($child->attributes()->value)) $value=$child->attributes()->value;
-				else $value=$option;
-				$selected="";
-				if(trim($e->populated_value['element_'.$e->id.'_'.$i]) == trim($option)) {
-					$selected = 'selected="selected"';
-				} elseif ($e->populated_value['element_'.$e->id.'_'.$i] == $value) {
-					$selected = 'selected="selected"';
+					$option=isset($t[0]) ? $t[0] : $child->__toString();
+					if (isset($child->attributes()->value)) $value=$child->attributes()->value;
+					else $value=$option;
+					$selected="";
+					if(trim($e->populated_value['element_'.$e->id.'_'.$i]) == trim($option)) {
+						$selected = 'selected="selected"';
+					} elseif ($e->populated_value['element_'.$e->id.'_'.$i] == $value) {
+						$selected = 'selected="selected"';
+					}
+					if (!$e->readonly || ($e->readonly && $selected)) $option_markup .= "<option value=\"".$value."\" {$selected}>".$option."</option>";
 				}
-				if (!$e->readonly || ($e->readonly && $selected)) $option_markup .= "<option value=\"".$value."\" {$selected}>".$option."</option>";
-			}
-		} elseif ($xmlf->fields->{'field'.$i}->values->attributes()->type=='range') {
-			$start=(int)$xmlf->fields->{'field'.$i}->values->attributes()->start;
-			$end=(int)$xmlf->fields->{'field'.$i}->values->attributes()->end;
-			for ($option=$start; $option <= $end; $option++) {
-				$selected="";
-				if(trim($e->populated_value['element_'.$e->id.'_'.$i]) == $option) {
-					$selected = 'selected="selected"';
+			} elseif ($xmlf->fields->{'field'.$i}->values->attributes()->type=='range') {
+				$start=(int)$xmlf->fields->{'field'.$i}->values->attributes()->start;
+				$end=(int)$xmlf->fields->{'field'.$i}->values->attributes()->end;
+				for ($option=$start; $option <= $end; $option++) {
+					$selected="";
+					if(trim($e->populated_value['element_'.$e->id.'_'.$i]) == $option) {
+						$selected = 'selected="selected"';
+					}
+					if (!$e->readonly || ($e->readonly && $selected)) $option_markup .= "<option value=\"".$option."\" {$selected}>".$option."</option>";
 				}
-				if (!$e->readonly || ($e->readonly && $selected)) $option_markup .= "<option value=\"".$option."\" {$selected}>".$option."</option>";
 			}
+			$field_markup.=$option_markup;
+			$field_markup.="</select>";
+		} else {
+			$field_markup.="<input id=\"element_{$e->id}_{$i}{$this->ail}\" name=\"element_{$e->id}_{$i}\" class=\"element text $this->classes\" size=\"{$this->size}\" value=\"{$e->values['element_'.$e->id.'_'.$i][$this->ai]}\" maxlength=\"{$this->maxlength}\" type=\"text\" {$readonly}/>";
 		}
-		$field_markup.=$option_markup;
-		$field_markup.="</select>";
 		$subscript_markup.="<label class=\"subname\" for=\"element_{$e->id}_{$i}\">".z_($xmlf->fields->{'field'.$i}->label)."</label>";
 
 		}
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: zingiri
 Donate link: http://www.zingiri.com/
 Tags: ecommerce, e-commerce, paypal, freewebshop, shop, cart, web shop, shopping cart, iDEAL, payson, Google Checkout, Worldpay, 2checkout, facebook, Payson, webshop, free web shop
 Requires at least: 2.1.7
-Tested up to: 3.8
-Stable tag: 2.6.6
+Tested up to: 3.8.1
+Stable tag: 2.6.7
 
 Zingiri Web Shop is a Wordpress plugin that adds fantastic ecommerce capabilities to a great content management system.
 
@@ -67,9 +67,12 @@ Before upgrading, make sure you back up your database first!
 
 == Changelog ==
 
+= 2.6.7 =
+* Fixed issue with drop downs not working on certain PHP installations
+
 = 2.6.6 =
 * Verified compatibility with Wordpress 3.8.1
-* Fixed security vulnerability
+* Fixed security vulnerability, thanks to  [Scott Schram](http://agilebrands.us)
 
 = 2.6.5 =
 * Verified compatibility with Wordpress 3.8
